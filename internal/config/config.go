b45ab464ae4efe837905ae9b4ebd15e921e91931
CRJL1001 <constantlescutprive2@gmail.com>
2026-08-20 13:58:06 +0200

    Configuration de la connexion à la bdd

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import ( //import
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct { //structure d'une configuration de connexion à la bdd
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+	Port       string
+}
+
+func LoadConfig() (*Config, error) { //charge les données d'environement dans une configuration
+	//chargement des variables d'environnement
+	if err := godotenv.Load(); err != nil {
+		log.Println("Aucun fichier .env trouvé, utilisation des variables locales")
+	}
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", ""),
+		DBPort:     getEnv("DB_PORT", ""),
+		DBUser:     getEnv("POSTGRES_USER", ""),
+		DBPassword: getEnv("POSTGRES_PASSWORD", ""),
+		DBName:     getEnv("POSTGRES_DB", ""),
+		DBSSLMode:  getEnv("DB_SSL_MODE", ""),
+		Port:       getEnv("PORT", ""),
+	}, nil
+}
+
+func getEnv(key, defaultValue string) string { //récupère les données d'environnement
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
