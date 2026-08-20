427bda49867e156a825572c0c2c553e6b2d99ab3
CRJL1001 <constantlescutprive2@gmail.com>
2026-08-20 13:46:37 +0200

    user models

---
+++ internal/models/user.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	Email     string    `json:"email" db:"email" validate:"required,email"`
+	Password  string    `json:"-" db:"password"` //- pour exclure du json
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type CreateUserRequest struct {
+	Email    string `json:"email" validate:"required,email"`    //format email requis
+	Password string `json:"password" validate:"required,min=8"` //minimum 8 caractères
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"` //format email requis
+	Password string `json:"password" validate:"required"`    //minimum 8 caractères
+}
